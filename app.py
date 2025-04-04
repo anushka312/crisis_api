21ffdcc2c3d605107d5300c11781ac7501184f17
Anushka <anushka031205@gmail.com>
2025-04-04 16:39:56 +0530

    fix

--- app.py
+++ app.py
@@ -43,3 +43,6 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
 
+@app.route('/')
+def home():
+    return "Crisis Detection API is running!"
